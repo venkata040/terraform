a82fdad209ad50b9fd866e77635923889e0177a6
venkataamarnath <venkatamar040@gmail.com>
2026-02-23 13:06:14 +1100

    terraform

---
+++ functions/ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "roboshop" {
+  count         = length(var.instances)
+  ami           = var.ami_id # left side and right no need to be same names
+  instance_type = var.instance_type
+  subnet_id     = "subnet-059c51047a06f132e"
+  vpc_security_group_ids = [ aws_security_group.allow_all_new.id ]
+
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+
+
+
+resource "aws_security_group" "allow_all_new" {
+    name        = var.sg_name
+    description = var.sg_description
+
+    ingress {
+      from_port        = var.from_port
+      to_port          = var.to_port
+      protocol         = "-1"
+      cidr_blocks      = var.cidr_blocks
+      ipv6_cidr_blocks = ["::/0"]
+  }
+    egress {
+      from_port        = var.from_port
+      to_port          = var.to_port
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+  }
+    tags  = var.sg_tags
+}
